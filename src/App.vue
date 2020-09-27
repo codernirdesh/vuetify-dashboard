1952f0d0840f92185e90af6e1771611360900022
Nirdesh Pokharel <mail@nirdeshpokhrel.com.np>
2020-09-27 09:04:47 +0545

    Initial APP

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-card class="mx-auto mt-5" width="400">
+      <v-card-title primary-title> Login </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field
+            prepend-icon="mdi-account-circle"
+            name="username"
+            label="Username"
+            id="username"
+            autofocus
+          ></v-text-field>
+          <v-select
+            v-model="e1"
+            :items="items"
+            label="State"
+            prepend-icon="mdi-media-network-outline"
+          ></v-select>
+          <v-text-field
+            prepend-icon="mdi-lock"
+            name="password"
+            label="Password"
+            id="password"
+            single-line
+            :type="showPassword ? 'text' : 'password'"
+            :append-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
+            @click:append="showPassword = !showPassword"
+          />
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-btn color="danger">Register</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn type="submit" color="success">
+              <v-icon>mdi-sign-in</v-icon> Login</v-btn
+            >
+          </v-card-actions>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  data: () => ({
+    showPassword: false,
+    items: [
+      "Provience 1",
+      "Provience 2",
+      "Provience 3",
+      "Provience 4",
+      "Provience 5",
+      "Provience 6",
+      "Provience 7",
+    ],
+  }),
+};
+</script>
